77e412dd7bea1bcd38295573ebe65aa3999eb598
siid sharma <siid123ck@gmail.com>
2021-08-19 15:59:30 +1000

    queue

--- test.js
+++ test.js
@@ -273,50 +273,93 @@
 
 // const list = new DoublyList(); 
 
-class Node{
-    constructor(val){
-        this.val=val;
-        this.next=null;
-    }
-}
+// class Node{
+//     constructor(val){
+//         this.val=val;
+//         this.next=null;
+//     }
+// }
 
-class Stack{
-    constructor(){
-        this.first= null;
-        this.last=null;
-        this.length=0;
-    }
+// class Stack{
+//     constructor(){
+//         this.first= null;
+//         this.last=null;
+//         this.length=0;
+//     }
 
-    push(val){
-        let newNode = new Node(val)
-        if (!this.first) {
-            this.first = newNode;
-            this.last=this.first;
-        } else {
-            let temp = this.first;
-            this.first = newNode;
-            newNode.next = temp;
-        }
-        this.length++;
-        return this.length;
-    }
+//     push(val){
+//         let newNode = new Node(val)
+//         if (!this.first) {
+//             this.first = newNode;
+//             this.last=this.first;
+//         } else {
+//             let temp = this.first;
+//             this.first = newNode;
+//             newNode.next = temp;
+//         }
+//         this.length++;
+//         return this.length;
+//     }
 
-    pop(){
-        if(this.length ===0) return undefined;
-        if(this.length===1){
-            this.first = null;
-            this.last = null;
-        }
-        let current = this.first;
-        this.first = current.next; 
-        this.length--;
-        return current;
-    }
-}
+//     pop(){
+//         if(this.length ===0) return undefined;
+//         if(this.length===1){
+//             this.first = null;
+//             this.last = null;
+//         }
+//         let current = this.first;
+//         this.first = current.next; 
+//         this.length--;
+//         return current;
+//     }
+// }
 
-let list = new Stack();
-list.push('first');
-list.push('second');
-list.push('third');
-list.push('fourth');
-console.log(list)
+// let list = new Stack();
+
+// class Node{
+//     constructor(val){
+//         this.val= val;
+//         this.next =null; 
+//     }
+// }
+
+// class Queue{
+//     constructor(){
+//         this.first=null;
+//         this.last = null;
+//         this.length = 0;
+//     }
+
+//     enqueue(val){
+//         let newNode = new Node(val);
+//         console.log(this.first)
+//         if(!this.first){
+//             this.first=newNode;
+//             this.last=newNode;
+//         }
+//         let lastNode = this.last;
+//         lastNode.next = newNode;
+//         this.last = newNode;
+//         this.length++;
+//         return this.length;
+//     }
+
+//     dequeue(){
+//         if(this.length ===0) return undefined;
+//         if(this.first===1){
+//             this.last = null;
+//         }
+//         let remove = this.first;
+//         this.first=remove.next;
+//         this.length--;
+//         return remove;
+//     }
+// }
+
+// let list = new Queue();
+// list.enqueue('first');
+// list.enqueue('second');
+// list.enqueue('third');
+// console.log(list)
+
+
